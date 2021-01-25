628a91a38a57cc7bfabcd9045c7764f3ed6291a7
opilane <opilane@HP-CZC6297B98>
2021-01-25 12:29:10 +0200

    6

---
+++ SQLiteDatabases/SQLiteDatabases/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SQLiteDatabases
+{
+    [Table("Friends")]
+    public class Friend
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+    }
+}
